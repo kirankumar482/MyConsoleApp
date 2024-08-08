ae11008954fe8be1df52d8216265b1202dc0590a
kirankumar482 <kiran.valuecreedintern2024@gmail.com>
2024-08-08 11:22:43 +0500

    added logic in program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Enter name");
+var name = Console.ReadLine();
+Console.WriteLine("Hello "+name);
+
